d1f4d6a776de7e20fb3344c466e97e5645fef3fc
Anik Hasan <playerspark666@gmail.com>
2026-02-13 23:02:53 +0600

    chore: stabilize monorepo, upgrade Next.js to 16.1.6, fix pnpm store and security audit

--- apps/web/next-env.d.ts
+++ apps/web/next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-// eslint-disable-next-line @nx/enforce-module-boundaries
 import "./../../dist/apps/web/.next/types/routes.d.ts";
 
 // NOTE: This file should not be edited
